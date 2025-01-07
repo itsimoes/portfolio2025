15fc9edf95bf8192ee3ba1574698956e0253c350
itsimoes <2d.italo@gmail.com>
2025-01-07 15:25:12 -0300

    clients component criado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Homepage from "@/components/Home/Homepage";
 import Portfolio from "@/components/Portfolio/Portfolio";
+import Clients from "@/components/Clients/Clients";
 
 export default function Home() {
   return (
     <>
       {/* <Homepage /> */}
-      <Portfolio />
+      {/* <Portfolio /> */}
+      <Clients />
     </>
   );
 }
---
+++ src/components/Clients/Clients.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./style.scss"
+import Image from 'next/image';
+import BarrosLogo from "../../../public/images/barros_logo.svg"
+import NSLogo from "../../../public/images/ns_logo.svg"
+
+const Clients = () => {
+    return (
+        <div className="clients_wrapper">
+            <div className="clients_container">
+                <div className="clients_title">
+                    <h2>Clientes.</h2>
+                    <div className="clients_underbar"></div>                    
+                </div>
+                <div className="clients_card_container">
+                   <a href="https://www.barrosconsultoriaeconcessoes.com/" target="_blank"><Image src={BarrosLogo} alt='' /></a>
+                   <a href="https://itsimoes.github.io/ns-advocacia/" target="_blank"><Image src={NSLogo} alt='' /></a>
+                </div>
+            </div>
+            <div className='clients_contato_link'>Contato.</div>
+        </div>
+    )
+}
+
+export default Clients
