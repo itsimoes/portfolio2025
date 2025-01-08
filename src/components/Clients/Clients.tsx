b28b6a667227116bce26c6d2331b1a8282c72818
itsimoes <2d.italo@gmail.com>
2025-01-08 16:43:03 -0300

    ajuste no layout do container principal

--- src/components/Clients/Clients.tsx
+++ src/components/Clients/Clients.tsx
@@ -10,14 +10,14 @@ const Clients = () => {
             <div className="clients_container">
                 <div className="clients_title">
                     <h2>Clientes.</h2>
-                    <div className="clients_underbar"></div>                    
+                    <div className="clients_underbar"></div>
                 </div>
                 <div className="clients_card_container">
-                   <a href="https://www.barrosconsultoriaeconcessoes.com/" target="_blank"><Image src={BarrosLogo} alt='' /></a>
-                   <a href="https://itsimoes.github.io/ns-advocacia/" target="_blank"><Image src={NSLogo} alt='' /></a>
+                    <a href="https://www.barrosconsultoriaeconcessoes.com/" target="_blank"><Image src={BarrosLogo} alt='' /></a>
+                    <a href="https://itsimoes.github.io/ns-advocacia/" target="_blank"><Image src={NSLogo} alt='' /></a>
                 </div>
+                <div className='clients_contato_link'>Contato.</div>
             </div>
-            <div className='clients_contato_link'>Contato.</div>
         </div>
     )
 }
