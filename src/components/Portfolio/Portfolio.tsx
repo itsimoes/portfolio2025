e9b312b3fed326c35d0365b1a0684e6e70857b69
itsimoes <2d.italo@gmail.com>
2025-01-08 20:56:02 -0300

    links e animations adicionados aos cards

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -11,11 +11,11 @@ const Portfolio = () => {
                     <h5>Projetos profissionais e pessoais.</h5>
                 </div>
                 <div className="portfolio_cards_container">
-                    <div id='portfolio_card_01' className="portfolio_card">Barros Consultoria</div>
-                    <div id='portfolio_card_02' className="portfolio_card">NS Advocacia</div>
-                    <div id='portfolio_card_03' className="portfolio_card">Restaurante</div>
-                    <div id='portfolio_card_04' className="portfolio_card">Clínica Veterinária</div>
-                    <div id='portfolio_card_05' className="portfolio_card">Trekking</div>
+                    <div id='portfolio_card_01' className="portfolio_card"><a href='https://www.barrosconsultoriaeconcessoes.com' target='_blank'><p>Barros Consultoria</p></a></div>
+                    <div id='portfolio_card_02' className="portfolio_card"><a href='https://itsimoes.github.io/ns-advocacia' target='_blank'><p>NS Advocacia</p></a></div>
+                    <div id='portfolio_card_03' className="portfolio_card"><a href='https://itsimoes.github.io/restaurante-01' target='_blank'><p>Restaurante</p></a></div>
+                    <div id='portfolio_card_04' className="portfolio_card"><a href='https://itsimoes.github.io/clinica-veterinaria' target='_blank'><p>Clínica Veterinária</p></a></div>
+                    <div id='portfolio_card_05' className="portfolio_card"><a href='https://itsimoes.github.io/trekking-01' target='_blank'><p>Trekking</p></a></div>
                     <div className="portfolio_card"></div>
                 </div>
             </div>
