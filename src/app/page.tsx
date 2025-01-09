9723b642333e68f4315510f0d336be56c9a85711
itsimoes <2d.italo@gmail.com>
2025-01-09 15:46:08 -0300

    support component criado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,16 @@ import Homepage from "@/components/Home/Homepage";
 import Portfolio from "@/components/Portfolio/Portfolio";
 import Clients from "@/components/Clients/Clients";
 import Navbar from "@/components/Navbar/Navbar";
+import Support from "@/components/Support/Support";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar />
       <Homepage />
       <Portfolio />
-      <Clients />
+      <Clients /> */}
+      <Support />
     </>
   );
 }
---
+++ src/components/Support/Support.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import "./style.scss"
+import Image from 'next/image';
+
+import sup01 from "../../../public/images/supportImg01.png"
+import sup02 from "../../../public/images/supportImg02.png"
+import img01 from "../../../public/images/support_img_01.png"
+import img02 from "../../../public/images/support_img_02.png"
+import img03 from "../../../public/images/support_img_03.png"
+
+const Support = () => {
+  return (
+    <div className="support_wrapper">
+      <div className="support_container">
+        <div className="support_card_container">
+          <div className="support_title">
+            <h2>Suporte de TI.</h2>
+            <div className="support_underbar"></div>
+            <p>Serviços de suporte de TI, como formatação, higienização de hardware, instalação de programas, consultoria na aquisição de hardware e software.</p>
+          </div>
+          <Image className="support_card_image" src={sup01} alt="" />
+        </div>
+        <div className="support_card_container">
+        <Image className="support_card_image" src={sup02} alt="" />
+          <div className="support_title_2">
+            <h2>Help Desk.</h2>
+            <div className="support_underbar_2"></div>
+            <p>Help desk avançado, auxiliando em situações de suporte remoto.</p>
+          </div>
+        </div>
+        <div className="support_images_container">
+        <Image className="support_image" src={img01} alt="" />
+        <Image className="support_image" src={img02} alt="" />
+        <Image className="support_image" src={img03} alt="" />
+
+        </div>
+        <div className='support_contato_link'>Contato.</div>
+      </div>
+    </div>
+  )
+}
+
+export default Support
