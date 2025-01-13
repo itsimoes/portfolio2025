934191dfbc6bcc725a8ee319ac2b733a59376408
itsimoes <2d.italo@gmail.com>
2025-01-13 15:41:30 -0300

    logo adicionada à home

--- src/components/Home/Homepage.tsx
+++ src/components/Home/Homepage.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./style.scss";
+import Image from 'next/image';
+import itsimoesLOGO from "../../../public/images/itsimoes_LOGO.svg"
 
 const Homepage = () => {
   return (
     <div className="home_wrapper">
       <div className="home_container">
-        <div className="homeimgmpcup"> {/*mocUp - home bg*/}
-
+        <div className="homebg"> {/*mocUp - home bg*/}
+          <Image className='homeLOGO' src={itsimoesLOGO} alt='' />
         </div>
         <h1 className="home_h1">Desenvolvedor Web. Suporte de TI. Help Desk.</h1>
       </div>
