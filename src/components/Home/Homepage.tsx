0b54b594772114a3166e94f5101ea9c1e0c83d2c
itsimoes <2d.italo@gmail.com>
2026-08-27 12:46:34 -0300

    botão de contato adicionado à home

--- src/components/Home/Homepage.tsx
+++ src/components/Home/Homepage.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./style.scss";
 import Image from 'next/image';
 import itsimoesLOGO from "../../../public/images/itsimoes_LOGO.svg"
+import Link from 'next/link'
 
 const Homepage = () => {
   return (
@@ -9,6 +10,7 @@ const Homepage = () => {
       <div className="home_container">
         <div className="homebg"> {/*mocUp - home bg*/}
           <Image className='homeLOGO' src={itsimoesLOGO} alt='' />
+          <Link href="/sobre&contato"><button className='homeButton'><span>Entre em contato</span></button></Link>
         </div>
         <h1 className="home_h1">Desenvolvedor Web. Suporte de TI. Help Desk.</h1>
       </div>
